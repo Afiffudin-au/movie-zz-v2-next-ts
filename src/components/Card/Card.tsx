72f14cfc091d00a462caaf8c7a6e2f6aff1c7a95
afiffudin-au <afiffudin077@gmail.com>
2021-02-24 13:14:08 +0700

    add skeleton

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import StarRateIcon from '@material-ui/icons/StarRate'
 import { amber } from '@material-ui/core/colors'
 import styles from './Card.module.scss'
 import { LightTooltip } from '../Mui-custom/LightTooltip/LightTooltip'
 import LazyLoad from 'react-lazyload'
 import { useRouter } from 'next/router'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 export interface CardOption {
   url?: string
   mediaType?: string
@@ -25,6 +26,8 @@ function Card({
   posterPath,
   voteAverage,
 }: CardOption) {
+  const [imageLoad, setImageLoad] = useState<boolean>(false)
+  const [display, setDisplay] = useState<string>('none')
   const router = useRouter()
   const handleDetail = () => {
     const query = router.query
@@ -34,26 +37,44 @@ function Card({
       query: query,
     })
   }
+  const handleImageLoad = () => {
+    setDisplay('block')
+    setImageLoad(true)
+  }
   return (
     <div className={styles.Card} style={styleProps}>
       <div onClick={handleDetail} className={styles.CardContent}>
+        {!imageLoad && (
+          <SkeletonTheme color='#455a64' highlightColor='#78909c'>
+            <Skeleton count={1} height={150} width={100} duration={1.5} />
+          </SkeletonTheme>
+        )}
         <LazyLoad>
           <LightTooltip
             enterDelay={10}
             title={originalTitle || ''}
             placement='top'
             arrow>
-            <img src={posterPath} alt={originalTitle} />
+            <img
+              style={{ display: display }}
+              src={posterPath}
+              alt={originalTitle}
+              onLoad={handleImageLoad}
+            />
           </LightTooltip>
         </LazyLoad>
-        <div className={styles.Card_desc}>
-          <h1>{originalTitle}</h1>
-          <p>{releaseDate}</p>
-        </div>
-        <div className={styles.CardStarRate}>
-          <StarRateIcon style={{ color: amber[600] }} />
-          <span>{voteAverage}</span>
-        </div>
+        {imageLoad && (
+          <>
+            <div className={styles.Card_desc}>
+              <h1>{originalTitle}</h1>
+              <p>{releaseDate}</p>
+            </div>
+            <div className={styles.CardStarRate}>
+              <StarRateIcon style={{ color: amber[600] }} />
+              <span>{voteAverage}</span>
+            </div>
+          </>
+        )}
       </div>
     </div>
   )
