87641e6f6ae5169840e859a7cdea2a07b7fdd937
afiffudin-au <afiffudin077@gmail.com>
2021-11-01 21:33:23 +0700

    package updates: sass@1.43.4, @types/node@16.11.6, @types/react@17.0.33, @types/react-dom@17.0.10, typescript@4.4.4,

--- next.config.js
+++ next.config.js
@@ -1,24 +1,32 @@
-
 module.exports = {
   env: {
-    API_KEY : 'f59a67c847f06eb38cff7065821c1fd9',
-    REACT_APP_POSTER_URL : 'https://www.themoviedb.org/t/p/w220_and_h330_face/',
-    REACT_APP_BIG_POSTER_URL : 'https://image.tmdb.org/t/p/original/',
-    REACT_APP_MOVIE_POPULAR : 'https://api.themoviedb.org/3/movie/popular?api_key=f59a67c847f06eb38cff7065821c1fd9',
-    REACT_APP_MOVIE_PLAYING : 'https://api.themoviedb.org/3/movie/now_playing?api_key=f59a67c847f06eb38cff7065821c1fd9',
-    REACT_APP_MOVIE_UPCOMING : 'https://api.themoviedb.org/3/movie/upcoming?api_key=f59a67c847f06eb38cff7065821c1fd9',
-    REACT_APP_TOP_RATED : 'https://api.themoviedb.org/3/movie/top_rated?api_key=f59a67c847f06eb38cff7065821c1fd9',
-    REACT_APP_TV_POPULAR : 'https://api.themoviedb.org/3/tv/popular?api_key=f59a67c847f06eb38cff7065821c1fd9',
-    REACT_APP_TV_AIRING_TODAY : 'https://api.themoviedb.org/3/tv/airing_today?api_key=f59a67c847f06eb38cff7065821c1fd9',
-    REACT_APP_TV_LATEST : 'https://api.themoviedb.org/3/tv/latest?api_key=f59a67c847f06eb38cff7065821c1fd9',
-    REACT_APP_TV_TOP_RATED : 'https://api.themoviedb.org/3/tv/top_rated?api_key=f59a67c847f06eb38cff7065821c1fd9',
+    API_KEY: 'f59a67c847f06eb38cff7065821c1fd9',
+    REACT_APP_POSTER_URL: 'https://www.themoviedb.org/t/p/w220_and_h330_face/',
+    REACT_APP_POSTER_URL_2ND: 'https://image.tmdb.org/t/p/original',
+    REACT_APP_BIG_POSTER_URL: 'https://image.tmdb.org/t/p/original/',
+    REACT_APP_MOVIE_POPULAR:
+      'https://api.themoviedb.org/3/movie/popular?api_key=f59a67c847f06eb38cff7065821c1fd9',
+    REACT_APP_MOVIE_PLAYING:
+      'https://api.themoviedb.org/3/movie/now_playing?api_key=f59a67c847f06eb38cff7065821c1fd9',
+    REACT_APP_MOVIE_UPCOMING:
+      'https://api.themoviedb.org/3/movie/upcoming?api_key=f59a67c847f06eb38cff7065821c1fd9',
+    REACT_APP_TOP_RATED:
+      'https://api.themoviedb.org/3/movie/top_rated?api_key=f59a67c847f06eb38cff7065821c1fd9',
+    REACT_APP_TV_POPULAR:
+      'https://api.themoviedb.org/3/tv/popular?api_key=f59a67c847f06eb38cff7065821c1fd9',
+    REACT_APP_TV_AIRING_TODAY:
+      'https://api.themoviedb.org/3/tv/airing_today?api_key=f59a67c847f06eb38cff7065821c1fd9',
+    REACT_APP_TV_LATEST:
+      'https://api.themoviedb.org/3/tv/latest?api_key=f59a67c847f06eb38cff7065821c1fd9',
+    REACT_APP_TV_TOP_RATED:
+      'https://api.themoviedb.org/3/tv/top_rated?api_key=f59a67c847f06eb38cff7065821c1fd9',
 
-    REACT_APP_POPULAR_PEOPLE : 'https://api.themoviedb.org/3/person/popular',
-    REACT_APP_PEOPLE_DETAIL : 'https://api.themoviedb.org/3/person/',
-    REACT_APP_MOVIE_DETAIL : 'https://api.themoviedb.org/3/movie/',
-    REACT_APP_TV_DETAIL : 'https://api.themoviedb.org/3/tv/'
+    REACT_APP_POPULAR_PEOPLE: 'https://api.themoviedb.org/3/person/popular',
+    REACT_APP_PEOPLE_DETAIL: 'https://api.themoviedb.org/3/person/',
+    REACT_APP_MOVIE_DETAIL: 'https://api.themoviedb.org/3/movie/',
+    REACT_APP_TV_DETAIL: 'https://api.themoviedb.org/3/tv/',
   },
   images: {
-    domains: ['image.tmdb.org','images.pexels.com'],
+    domains: ['image.tmdb.org', 'images.pexels.com'],
   },
 }
